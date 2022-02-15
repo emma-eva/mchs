4c4736b65b0672cfba868d5d5b4c80bde10e72d5
Emma Eva <67018591+emma-eva@users.noreply.github.com>
2022-02-15 20:29:28 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,51 +1,61 @@
 #!/usr/bin/bash
 
-if [ ! -d ~/mchs/mchspkg ];
-then
-    mkdir -p ~/mchs/mchspkg/DEBIAN ~/mchs/mchspkg$PREFIX/bin
-	touch ~/mchs/mchspkg/DEBIAN/control
-fi
-read -p 'Enter direct deb file path: ' DEBURL
-if [[ $DEBURL =~ https?:// ]]
-then
-	wget -P ~/mchs/debs $DEBURL
-	ar x ~/mchs/debs/*.deb --output ~/mchs/debs
-	mkdir ~/mchs/debs/control
-	if [ -f ~/mchs/debs/control.tar.xz ];
+SDCARD_DEB_PATH=/sdcard/McHs/pool/main
+MCHS_PREFIX='/data/data/com.mchs/files/usr'
+
+for DEB_FILES in $SDCARD_DEB_PATH/*/*/*.deb;
+do
+	DEB_PATH=(${DEB_FILES//\// })
+	DEB_URL=$SDCARD_DEB_PATH/${DEB_PATH[4]}/${DEB_PATH[5]}/${DEB_PATH[6]}
+	DEB_FILE_PATH=$SDCARD_DEB_PATH/${DEB_PATH[4]}/${DEB_PATH[5]}
+
+	if [ ! -d ~/build/mchspkg ];
 	then
-		tar -xf ~/mchs/debs/control.tar.xz -C ~/mchs/debs/control
-	else
-		tar -xf ~/mchs/debs/control.tar.gz -C ~/mchs/debs/control
+		mkdir -p ~/build/mchspkg/DEBIAN ~/build/mchspkg$MCHS_PREFIX/bin
+		touch ~/build/mchspkg/DEBIAN/control
 	fi
-	if [ -f ~/mchs/mchspkg/DEBIAN/control ];
+	mkdir ~/build/debs
+	cp $DEB_URL ~/build/debs
+	ar x ~/build/debs/*.deb --output ~/build/debs
+	mkdir ~/build/debs/control
+	if [ -f ~/build/debs/control.tar.xz ];
 	then
-		rm -rf ~/mchs/mchspkg/DEBIAN/*
-		mv ~/mchs/debs/control/* ~/mchs/mchspkg/DEBIAN
+		tar -xf ~/build/debs/control.tar.xz -C ~/build/debs/control
 	else
-		IN="${DEBURL##*/}"
+		tar -xf ~/build/debs/control.tar.gz -C ~/build/debs/control
+	fi
+	if [ -f ~/build/mchspkg/DEBIAN/control ];
+	then
+		rm -rf ~/build/mchspkg/DEBIAN/*
+		mv ~/build/debs/control/* ~/build/mchspkg/DEBIAN
+	else
+		IN="${DEB_URL##*/}"
 		arrIN=(${IN//_/ })
-		touch ~/mchs/mchspkg/DEBIAN/control
-		printf "Package: ${arrIN[0]}\nArchitecture: ${arrIN[2]:0:-4}\nMaintainer: @MCHS\nVersion: ${arrIN[1]}\nHomepage: https://tuberboy.com/MCHS\nDescription: Not Found Enough Information" >> ~/mchs/mchspkg/DEBIAN/control
+		touch ~/build/mchspkg/DEBIAN/control
+		printf "Package: ${arrIN[0]}\nArchitecture: ${arrIN[2]:0:-4}\nMaintainer: @MCHS\nVersion: ${arrIN[1]}\nHomepage: https://tuberboy.com/MCHS\nDescription: Not Found Enough Information" >> ~/build/mchspkg/DEBIAN/control
 	fi
-	if [ -f ~/mchs/debs/data.tar.xz ];
+	if [ -f ~/build/debs/data.tar.xz ];
 	then
-		tar -xf ~/mchs/debs/data.tar.xz -C ~/mchs/debs
+		tar -xf ~/build/debs/data.tar.xz -C ~/build/debs
 	else
-		tar -xf ~/mchs/debs/data.tar.gz -C ~/mchs/debs
+		tar -xf ~/build/debs/data.tar.gz -C ~/build/debs
 	fi
-	rm -rf ~/mchs/mchspkg$PREFIX/*
-	mv ~/mchs/debs/data/data/*/files/usr/* ~/mchs/mchspkg$PREFIX/
-	echo 'Building MCHS Package...'
+
+	rm -rf ~/build/mchspkg$MCHS_PREFIX/*
+	mv ~/build/debs/data/data/*/files/usr/* ~/build/mchspkg$MCHS_PREFIX/
+	echo '\n\n Building McHs Package...'
 	echo ' Please wait...'
-	find ~/mchs/mchspkg/DEBIAN/* -type f -exec sed -i -e 's/com.termux/io.neoterm/g' {} \;
-	find ~/mchs/mchspkg/DEBIAN/* -type f -exec sed -i -e 's/@termux/@MCHS/g' {} \;
-	find ~/mchs/mchspkg$PREFIX/./ -type f -exec sed -i -e 's/com.termux/io.neoterm/g' {} \;
-	#find ~/mchs/mchspkg$PREFIX/./ -type f -exec sed -i -e 's/termux-build/mchs-build/g' {} \;
-	chmod 755 ~/mchs/mchspkg/DEBIAN/*
-	chmod 755 ~/mchs/mchspkg/DEBIAN
-	chmod +x ~/mchs/mchspkg$PREFIX/bin/*
-	dpkg-deb --build ~/mchs/mchspkg ${DEBURL##*/}
-	rm -rf ~/mchs/debs
-else
-	echo -e '\e[31mError:\e[0m Please enter a valid url with http(s)\n'
-fi
+	find ~/build/mchspkg/DEBIAN/* -type f -exec sed -i -e 's/io.neoterm/com.mchs/g' {} \;
+	find ~/build/mchspkg/DEBIAN/* -type f -exec sed -i -e 's/@termux/@McHs/g' {} \;
+	find ~/build/mchspkg$MCHS_PREFIX/./ -type f -exec sed -i -e 's/io.neoterm/com.mchs/g' {} \;
+	#find ~/build/mchspkg$MCHS_PREFIX/./ -type f -exec sed -i -e 's/termux-build/mchs-build/g' {} \;
+	chmod 755 ~/build/mchspkg/DEBIAN/*
+	chmod 755 ~/build/mchspkg/DEBIAN
+	chmod +x ~/build/mchspkg$MCHS_PREFIX/bin/*
+	dpkg-deb --build ~/build/mchspkg ${DEB_URL##*/}
+	rm -rf ~/build/debs
+	rm -rf ~/build/mchspkg
+	echo '\n Build Finished Successfully'
+	mv ${DEB_PATH[6]} $DEB_FILE_PATH
+	printf ' Moved File: %s To: %s\n\n' "${DEB_PATH[6]}" "$DEB_FILE_PATH"
+done
